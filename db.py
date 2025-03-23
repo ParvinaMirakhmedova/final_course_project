e1ec76c9feeb9af160af35fca2d4649504b47bde
Parvina <mirakhmedova89@gmail.com>
2025-03-23 12:19:52 +0500

    update the code in main.py

--- db.py
+++ db.py
@@ -4,11 +4,9 @@ import pandas as pd
 DB_FILE = "my.db"
 
 
-def fetch_patients() -> pd.DataFrame:
-    """
-    Получает объединённые данные из представления view1_patients_1
-    и возвращает их в виде DataFrame для анализа и визуализации.
-    """
+def fetch_patients() -> pd.DataFrame:#Получает объединённые данные из представления view1_patients_1 и 
+    #возвращает их в виде DataFrame для анализа и визуализации.
+
     with duckdb.connect(DB_FILE) as duck:
         df = duck.execute("SELECT * FROM view1_patients_1").fetchdf()
         return df
